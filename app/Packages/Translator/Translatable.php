575dcf7f2977418ea97c2e857e8298385a721e87
Voydz <voydz@hotmail.com>
2015-03-22 18:20:45 +0100

    - new languageing!!

---
+++ app/Packages/Translator/Translatable.php
@@ -0,0 +1,27 @@
+<?php namespace Vain\Packages\Translator;
+
+interface Translatable {
+
+    /**
+     * content relation to all locales
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contents();
+
+    /**
+     * specific content relation by locale
+     *
+     * @param null $locale
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function content($locale = null);
+
+    /**
+     * accessor for content by locale
+     *
+     * @param $value
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getContentAttribute($value);
+}
