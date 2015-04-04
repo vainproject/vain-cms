c0bd62e1c412e8e96baf73fa25e2f19e715e6ec5
Florian Greinus <florian.greinus@gmail.com>
2015-04-05 00:10:04 +0200

    [Blog] add migration for post softdeletes

---
+++ modules/Blog/Database/Migrations/2015_04_05_000906_add_soft_deletes_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeletesToPostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('posts', function(Blueprint $table)
+		{
+			$table->dropSoftDeletes();
+		});
+	}
+
+}
