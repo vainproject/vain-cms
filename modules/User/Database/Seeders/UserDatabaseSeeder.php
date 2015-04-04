1efe09828185f524b26dbce4f646ad5e151b4d03
ottowayne <mr.ottowayne@gmail.com>
2015-04-04 21:40:05 +0200

    [User] add few more account seeds with actual email addresses

--- modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -7,7 +7,8 @@ use Modules\User\Entities\Permission;
 use Modules\User\Entities\Role;
 use Modules\User\Entities\User;
 
-class UserDatabaseSeeder extends Seeder {
+class UserDatabaseSeeder extends Seeder
+{
 
     /**
      * Run the database seeds.
@@ -26,29 +27,61 @@ class UserDatabaseSeeder extends Seeder {
     }
 }
 
-class UserTableSeeder extends Seeder {
+class UserTableSeeder extends Seeder
+{
 
     public function run()
     {
         DB::table('users')->delete();
 
-        $user = User::create([
-            'id' => 1,
-            'name' => 'Admin',
-            'email' => 'admin@vain.app',
+        $admin = User::create([
+            'id'       => 1,
+            'name'     => 'admin',
+            'email'    => 'admin@vain.app',
             'password' => bcrypt('123456'),
             'locale' => 'en',
             'birthday_at' => '2000-01-01',
         ]);
 
+        $fgreinus = User::create([
+            'id'       => 2,
+            'name'     => 'fgreinus',
+            'email'    => 'florian.greinus@gmail.com',
+            'password' => bcrypt('123456'),
+            'locale'   => 'de',
+            'birthday_at' => '2000-01-01',
+        ]);
+
+        $voydz = User::create([
+            'id'       => 3,
+            'name'     => 'voydz',
+            'email'    => 'voydz@hotmail.com',
+            'password' => bcrypt('123456'),
+            'locale'   => 'de',
+            'birthday_at' => '2000-01-01',
+        ]);
+
+        $ottowayne = User::create([
+            'id'       => 4,
+            'name'     => 'ottowayne',
+            'email'    => 'mr.ottowayne@gmail.com',
+            'password' => bcrypt('123456'),
+            'locale'   => 'de',
+            'birthday_at' => '2000-01-01',
+        ]);
+
         // add user to admin roles
+        /** @var Role[] $adminRoles */
         $adminRoles = Role::where('name', 'administrator')->get();
-        $user->saveRoles($adminRoles);
+        $admin->saveRoles($adminRoles);
+        $fgreinus->saveRoles($adminRoles);
+        $voydz->saveRoles($adminRoles);
+        $ottowayne->saveRoles($adminRoles);
 
         // add all permissions to admin roles
+        /** @var Permission[] $permissions */
         $permissions = Permission::all();
-        foreach ($adminRoles as $role)
-        {
+        foreach ($adminRoles as $role) {
             $role->savePermissions($permissions);
         }
     }
