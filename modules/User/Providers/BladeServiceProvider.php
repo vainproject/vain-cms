337e57e0a25accaf6c0417a746fb58238c0630fa
Florian Greinus <florian.greinus@gmail.com>
2015-11-29 00:55:12 +0100

    * fix blade userbadge directive to handle users without roles

--- modules/User/Providers/BladeServiceProvider.php
+++ modules/User/Providers/BladeServiceProvider.php
@@ -8,7 +8,8 @@ class BladeServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::directive('userbadge', function($expression) {
-            return "<span class=\"label label-role role-<?php echo with{$expression}->roles()->ordered()->first()->color; ?>\"><?php echo with{$expression}->name; ?></span>";
+
+            return "<span class=\"label label-role role-<?php echo with{$expression}->roles()->count() > 0 ? with{$expression}->roles()->ordered()->first()->color : ''; ?>\"><?php echo with{$expression}->name; ?></span>";
         });
     }
 
