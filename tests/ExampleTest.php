991793644bf629c802ae6bad899782f16f54b4e0
voydz <voydz@hotmail.com>
2015-12-11 17:25:58 +0100

    - updated phpunit to version 5 (implicitly supporting php7) - updated unit tests to default to current laravel tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,7 +1,11 @@
 <?php
 
-class ExampleTest extends TestCase {
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+class ExampleTest extends TestCase
+{
     /**
      * A basic functional test example.
      *
@@ -9,9 +13,7 @@ class ExampleTest extends TestCase {
      */
     public function testBasicExample()
     {
-        $response = $this->call('GET', '/');
-
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->visit('/')
+            ->see('Vain.');
     }
-
 }
