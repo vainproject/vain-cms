17f5ab91be21ece3e8a8ba67ba5fc8d8e6805900
voydz <voydz@hotmail.com>
2016-10-20 15:53:26 +0200

    proof-of-concept: dezentralized asset management

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,6 @@
 process.env.DISABLE_NOTIFIER = true;
 var elixir = require('laravel-elixir');
 
-var scripts_include = [
-    /*
-     |--------------------------------------------------------------------------
-     | Vendor Javascript
-     |--------------------------------------------------------------------------
-     */
-    './node_modules/jquery/dist/jquery.js',
-    './node_modules/bootstrap/dist/js/bootstrap.js',
-    './node_modules/toastr/toastr.js',
-    './node_modules/jquery-treegrid/js/jquery.treegrid.js',
-    './node_modules/jquery-treegrid/js/jquery.treegrid.bootstrap3.js',
-    './node_modules/bootstrap-select/dist/js/bootstrap-select.js',
-    './node_modules/wowjs/dist/wow.js',
-
-    /*
-     |--------------------------------------------------------------------------
-     | App Javascript
-     |--------------------------------------------------------------------------
-     */
-    './resources/assets/js/app.js',
-    './resources/assets/js/modal.js',
-    './resources/assets/js/notify.js',
-    './resources/assets/js/pjax.js',
-    './resources/assets/js/hero.js',
-    './resources/assets/js/parallax.js',
-    './resources/assets/js/core.js',
-    './resources/assets/js/frontend.js',
-
-    /*
-     |--------------------------------------------------------------------------
-     | Module Javascript
-     |--------------------------------------------------------------------------
-     */
-    //'./modules/Forum/Resources/assets/js/*.js',
-    //'./modules/Site/Resources/assets/js/*.js',
-    //'./modules/User/Resources/assets/js/*.js',
-];
-
-var scripts_admin_include = [
-    /*
-     |--------------------------------------------------------------------------
-     | Vendor Javascript
-     |--------------------------------------------------------------------------
-     */
-    './node_modules/admin-lte/dist/js/app.js',
-    './resources/assets/js/search.js'
-
-    /*
-     |--------------------------------------------------------------------------
-     | Module Javascript
-     |--------------------------------------------------------------------------
-     */
-    //'./modules/User/Resources/assets/js/admin/*.js',
-];
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -67,30 +12,13 @@ var scripts_admin_include = [
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
 
-    // compile less
-    mix.less(['app.less', 'backend.less'], 'public/static/css/backend.css');
-    mix.less(['app.less', 'frontend.less'], 'public/static/css/frontend.css');
-
-    // register custom watcher for less task
-    elixir.Task.find('less').watchers.push('./modules/**/*.less');
-
-    // concat scripts
-    mix.scripts(scripts_include, 'public/static/js/app.js', './');
-    mix.scripts(scripts_admin_include, 'public/static/js/admin.js', './');
-    // NOTE: watcher for every given script are added automatically
-
-    // copy fonts
-    mix.copy('./node_modules/bootstrap/dist/fonts', 'public/static/fonts');
-    mix.copy('./node_modules/font-awesome/fonts', 'public/static/fonts');
-    mix.copy('./resources/assets/fonts', 'public/static/fonts');
-
-    // copy images
-    mix.copy('./resources/assets/img', 'public/static/images');
-
-    // copy admin statics
-    //mix.copy('./node_modules/admin-lte/plugins', 'public/static/plugins');
+    require('vain')(mix, {
+        'extra_less': [
+            './resources/assets/less/modules.less'
+        ]
+    });
 
     // versioning files
     //mix.version([
